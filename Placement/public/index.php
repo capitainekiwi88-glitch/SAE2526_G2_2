27a425bbae5558d4ef3db29e90ff34405fbf5627
capitainekiwi88-glitch <capitainekiwi88@gmail.com>
2026-03-06 00:33:31 +0100

    install & configure twig (Vendor) base and seperate old and new directories

---
+++ Placement/public/index.php
@@ -0,0 +1,13 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
+$twig = new \Twig\Environment($loader, [
+    'cache' => false,
+]);
+
+echo $twig->render('index.html.twig', [
+    'nom_projet' => 'Gestion de Placement',
+    'etudiants' => ['Alice', 'Bob', 'Charlie'],
+    'message' => 'Ton installation Twig est un succès !'
+]);
