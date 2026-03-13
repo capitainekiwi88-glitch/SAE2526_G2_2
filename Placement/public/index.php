23e87ea4ed3f520523c0f8cc7a4133e3de70dffe
stake <stake@tutanota.de>
2026-03-13 14:39:36 +0100

    Ajout système de création de salle (4 étapes) + routage

--- Placement/public/index.php
+++ Placement/public/index.php
@@ -6,8 +6,17 @@ $twig = new \Twig\Environment($loader, [
     'cache' => false,
 ]);
 
-echo $twig->render('index.html.twig', [
-    'nom_projet' => 'Gestion de Placement',
-    'etudiants' => ['Alice', 'Bob', 'Charlie'],
-    'message' => 'Ton installation Twig est un succès !'
-]);
+$page = $_GET['p'] ?? 'home';
+
+switch ($page) {
+    case 'gest_salle':
+        echo $twig->render('salle.html.twig', []);
+        break;
+    default:
+        echo $twig->render('index.html.twig', [
+            'nom_projet' => 'Gestion de Placement',
+            'etudiants' => ['Alice', 'Bob', 'Charlie'],
+            'message' => 'Ton installation Twig est un succès !'
+        ]);
+        break;
+}
